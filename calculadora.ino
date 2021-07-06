9387b61a2f1025a605ac3a27d7d3670f9fdc95ca
Saltos99 <87043479+Saltos99@users.noreply.github.com>
2021-07-06 17:07:58 -0500

    Mensajes de solicitud de datos para la calculadora

---
+++ calculadora.ino
@@ -0,0 +1,43 @@
+#include<iostream.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<math.h>
+
+double Suma (float a, float b){
+  result = a+b;
+  return result;
+}
+
+double Resta (float a, float b){
+  result = a-b;
+  return result;
+}
+
+double Mult (float a, float b){
+  result = a*b;
+  return result;
+}
+
+double Div (float a, float b){
+  result = a/b;
+  return result;
+}
+
+void setup() {
+}
+
+void loop() {
+  float a, b;
+  char op;
+  double result;
+
+  cout<<"Ingrese primer numero: ";
+  cin>>n1;
+  cout<<"Ingrese primer numero: ";
+  cin>>n2;
+  cout<<"Ingrese operacion que gusta realizar (+,-,*,/): ";
+  cin>>op;
+
+    
+  }
+}
