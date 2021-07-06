eaf9d3dc09e302f8b90e768a5b8ac29b73e3b6b8
JDOG99 <86674039+JDOG99@users.noreply.github.com>
2021-07-06 17:17:21 -0500

    Versión final

--- calculadora.ino
+++ calculadora.ino
@@ -23,21 +23,24 @@ double Div (float a, float b){
   return result;
 }
 
-void setup() {
-}
-
-void loop() {
+void main() {
   float a, b;
   char op;
   double result;
 
   cout<<"Ingrese primer numero: ";
   cin>>n1;
-  cout<<"Ingrese primer numero: ";
+  cout<<"Ingrese segundo numero: ";
   cin>>n2;
   cout<<"Ingrese operacion que gusta realizar (+,-,*,/): ";
   cin>>op;
-
-    
+  switch(op){case '+': double Suma(a, b); break;
+             case '-': double Resta(a, b); break;
+             case '*': double Mult(a, b); break;
+             case '/': double Div(a, b); break;
+             default: cout<<"Operador no definido";
+             exit(1);
+             }
+  cout<<"El resultado es: "<<result;
   }
 }
