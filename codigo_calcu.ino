ce2bbe02cdf40be8b0c461e738ad032786726b21
OctavioRoman <86981028+OctavioRoman@users.noreply.github.com>
2021-07-06 15:08:08 -0500

    Librería y variables a utilizar

---
+++ codigo_calcu.ino
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<math.h>
+
+void setup() {
+  float a, b;
+  char op;
+  double result;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
